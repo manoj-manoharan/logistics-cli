3044983e063a83a64b62a59b5a3b4cc3d310a439
Manoj <yogeshwaranmanoharan@gmail.com>
2024-03-07 20:12:58 +0530

    - Simplifying in range test case

--- __tests__/in_range.test.ts
+++ __tests__/in_range.test.ts
@@ -2,23 +2,15 @@ import { inRange } from '../src/util/inRange.js';
 
 describe('Testing is whole number function', () => {
   //
-  it('1, 10, 5 should be true', () => {
-    expect(inRange({ min: 1, max: 10, val: 5 })).toBe(true);
+  it('-100, 0, 100 should be true', () => {
+    expect(inRange({ min: -100, val: 0, max: 100 })).toBe(true);
   });
 
-  it('1, 10, 11 should be false', () => {
-    expect(inRange({ min: 1, max: 10, val: 11 })).toBe(false);
+  it('-100, -101, 100 should be false', () => {
+    expect(inRange({ min: -100, val: -101, max: 100 })).toBe(false);
   });
 
-  it('0, 10, 1 should be true', () => {
-    expect(inRange({ min: 0, max: 10, val: 1 })).toBe(true);
-  });
-
-  it('-1, 10, 1 should be true', () => {
-    expect(inRange({ min: -1, max: 10, val: 1 })).toBe(true);
-  });
-
-  it('-1, 10, -2 should be false', () => {
-    expect(inRange({ min: -1, max: 10, val: -2 })).toBe(false);
+  it('-100, 101, 100 should be false', () => {
+    expect(inRange({ min: -100, val: 101, max: 100 })).toBe(false);
   });
 });
